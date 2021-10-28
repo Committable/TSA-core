fe9ebd19c0b673ad48953880cf4bb2ae8c34121d
yangzq12 <softwareyangzq12@163.com>
2021-10-28 10:02:54 +0800

    debug meta commit for ast

--- inputDealer/solidityAstWalker.py
+++ inputDealer/solidityAstWalker.py
@@ -9,6 +9,12 @@ class AstWalker:
                 self._walk_with_attrs_legacy(node, attributes, nodes)
             else:
                 self._walk_with_list_of_attrs_legacy(node, attributes, nodes)
+        if self.type == "ast":
+            if isinstance(attributes, dict):
+                self._walk_with_attrs(node, attributes, nodes)
+            else:
+                self._walk_with_list_of_attrs(node, attributes, nodes)
+
 
     def walk_to_json(self, source, node, depth):
         json_result = {}
@@ -97,6 +103,35 @@ class AstWalker:
                 for child in node["children"]:
                     self._walk_with_list_of_attrs_legacy(child, list_of_attributes, nodes)
 
+    def _walk_with_attrs(self, node, attributes, nodes):
+        if self._check_attributes(node, attributes):
+            nodes.append(node)
+        else:
+            if "nodes" in node and node["nodes"]:
+                for node in node["nodes"]:
+                    self._walk_with_attrs(node, attributes, nodes)
+            if "body" in node and node["body"]:
+                for node in node["body"]:
+                    self._walk_with_attrs(node, attributes, nodes)
+            if "statements" in node and node["statements"]:
+                for node in node["statements"]:
+                    self._walk_with_attrs(node, attributes, nodes)
+            if "trueBody" in node and node["trueBody"]:
+                for node in node["trueBody"]:
+                    self._walk_with_attrs(node, attributes, nodes)
+            if "falseBody" in node and node["falseBody"]:
+                for node in node["falseBody"]:
+                    self._walk_with_attrs(node, attributes, nodes)
+
+
+    def _walk_with_list_of_attrs(self, node, list_of_attributes, nodes):
+        if self._check_list_of_attributes(node, list_of_attributes):
+            nodes.append(node)
+        else:
+            if "children" in node and node["children"]:
+                for child in node["children"]:
+                    self._walk_with_list_of_attrs_legacy(child, list_of_attributes, nodes)
+
     def _check_attributes_legacy(self, node, attributes):
         for name in attributes:
             if name == "attributes":
@@ -109,8 +144,26 @@ class AstWalker:
                     return False
         return True
 
+    def _check_attributes(self, node, attributes):
+        for name in attributes:
+            if name == "attributes":
+                if "attributes" not in node or \
+                        not self._check_attributes(node["attributes"],
+                                                          attributes["attributes"]):
+                    return False
+            else:
+                if name not in node or node[name] != attributes[name]:
+                    return False
+        return True
+
     def _check_list_of_attributes_legacy(self, node, list_of_attributes):
         for attrs in list_of_attributes:
             if self._check_attributes_legacy(node, attrs):
+                return True
+        return False
+
+    def _check_list_of_attributes(self, node, list_of_attributes):
+        for attrs in list_of_attributes:
+            if self._check_attributes(node, attrs):
                 return True
         return False
